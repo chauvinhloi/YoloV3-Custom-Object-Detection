84dcab7796e883b9d462a7992f9883e36096fa8f
Vinh Loi Chau <chauvinhloi@gmail.com>
2020-08-01 10:04:51 +1000

    train test generated

--- training/train_test.py
+++ training/train_test.py
@@ -2,7 +2,7 @@ import glob
 import os
 
 # put your own path here
-dataset_path = '/home/thecaffeinedev/YoloV3-Custom-Object-Detection/training/images'
+dataset_path = './images'
 
 # Percentage of images to be used for the test set
 percentage_test = 25
